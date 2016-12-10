8a74d5313b6747462775416a3b6861a0c9c6929e
Ammo Reyes <amoreyes@gmail.com>
2016-12-09 21:28:12 -0500

    initial commit

---
+++ app/index.js
@@ -0,0 +1,13 @@
+var React = require('react')
+var ReactDom = require('react-dom')
+var HelloWorld = React.createClass({
+  render : function () {
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  }
+});
+
+ReactDom.render(<HelloWorld />, document.getElementById('app'));
